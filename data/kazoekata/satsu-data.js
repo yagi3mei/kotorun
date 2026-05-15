6617a6f6365009f7fe735719c69789befa6193da
yagi3mei <141975956+yagi3mei@users.noreply.github.com>
2026-05-15 11:42:49 +0900

    Add multilingual in-app browser warning 5

---
+++ data/kazoekata/satsu-data.js
@@ -0,0 +1,172 @@
+export default [
+    {
+        id: "issatsu",
+        number: 1,
+        counter: "さつ",
+        questionSpeech: "なんさつ",
+        reading: "いっさつ",
+        speech: "いっさつ",
+        item: "ほん",
+        img: "hon_1.png",
+
+        choices: [
+            "いっさつ",
+            "いちさつ",
+            "ひとさつ",
+            "いっぽん"
+        ]
+    },
+    {
+        id: "nisatsu",
+        number: 2,
+        counter: "さつ",
+        questionSpeech: "なんさつ",
+        reading: "にさつ",
+        speech: "にさつ",
+        item: "ほん",
+        img: "hon_2.png",
+
+        choices: [
+            "にさつ",
+            "ふたさつ",
+            "さんさつ",
+            "にほん"
+        ]
+    },
+    {
+        id: "sansatsu",
+        number: 3,
+        counter: "さつ",
+        questionSpeech: "なんさつ",
+        reading: "さんさつ",
+        speech: "さんさつ",
+        item: "ほん",
+        img: "hon_3.png",
+
+        choices: [
+            "さんさつ",
+            "さんざつ",
+            "みさつ",
+            "さんぼん"
+        ]
+    },
+    {
+        id: "yonsatsu",
+        number: 4,
+        counter: "さつ",
+        questionSpeech: "なんさつ",
+        reading: "よんさつ",
+        speech: "よんさつ",
+        item: "ほん",
+        img: "hon_4.png",
+
+        choices: [
+            "よんさつ",
+            "しさつ",
+            "ごさつ",
+            "よんほん"
+        ]
+    },
+    {
+        id: "gosatsu",
+        number: 5,
+        counter: "さつ",
+        questionSpeech: "なんさつ",
+        reading: "ごさつ",
+        speech: "ごさつ",
+        item: "ほん",
+        img: "hon_5.png",
+
+        choices: [
+            "ごさつ",
+            "いつさつ",
+            "ろくさつ",
+            "ごほん"
+        ]
+    },
+    {
+        id: "rokusatsu",
+        number: 6,
+        counter: "さつ",
+        questionSpeech: "なんさつ",
+        reading: "ろくさつ",
+        speech: "ろくさつ",
+        item: "ほん",
+        img: "hon_6.png",
+
+        choices: [
+            "ろくさつ",
+            "むっさつ",
+            "ななさつ",
+            "ろっぽん"
+        ]
+    },
+    {
+        id: "nanasatsu",
+        number: 7,
+        counter: "さつ",
+        questionSpeech: "なんさつ",
+        reading: "ななさつ",
+        speech: "ななさつ",
+        item: "ほん",
+        img: "hon_7.png",
+
+        choices: [
+            "ななさつ",
+            "しちさつ",
+            "はっさつ",
+            "ななほん"
+        ]
+    },
+    {
+        id: "hassatsu",
+        number: 8,
+        counter: "さつ",
+        questionSpeech: "なんさつ",
+        reading: "はっさつ",
+        speech: "はっさつ",
+        item: "ほん",
+        img: "hon_8.png",
+
+        choices: [
+            "はっさつ",
+            "はちさつ",
+            "きゅうさつ",
+            "はっぽん"
+        ]
+    },
+    {
+        id: "kyuusatsu",
+        number: 9,
+        counter: "さつ",
+        questionSpeech: "なんさつ",
+        reading: "きゅうさつ",
+        speech: "きゅうさつ",
+        item: "ほん",
+        img: "hon_9.png",
+
+        choices: [
+            "きゅうさつ",
+            "ここのさつ",
+            "くさつ",
+            "きゅうほん"
+        ]
+    },
+    {
+        id: "jussatsu",
+        number: 10,
+        counter: "さつ",
+        questionSpeech: "なんさつ",
+        reading: "じゅっさつ",
+        speech: "じゅっさつ",
+        item: "ほん",
+        img: "hon_10.png",
+
+        choices: [
+            "じゅっさつ",
+            "じゅうさつ",
+            "じゅっほん",
+            "じゅうほん"
+        ]
+    }
+];
