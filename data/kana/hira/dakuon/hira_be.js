d628aada945cee6c54722d36c0dbde375fc3507c
yagi3mei <141975956+yagi3mei@users.noreply.github.com>
2026-07-04 14:18:14 +0900

    karuta-game fix group

--- data/kana/hira/dakuon/hira_be.js
+++ data/kana/hira/dakuon/hira_be.js
@@ -1,5 +1,4 @@
 export default [
-
     {
         id: "benkyou_study",
         word: "べんきょう",
@@ -9,7 +8,6 @@ export default [
         img: "benkyou_study.png",
         lesson: 8
     },
-
     {
         id: "bentou_lunchbox",
         word: "べんとう",
@@ -19,7 +17,6 @@ export default [
         img: "bentou_lunchbox.png",
         lesson: 24
     },
-
     {
         id: "benrina_convenient",
         word: "べんりな",
@@ -28,6 +25,23 @@ export default [
         sentence: "べんりな スマホです。",
         img: "benrina_convenient.png",
         lesson: 8
+    },
+    {
+        id: "boushi_cap",
+        word: "ぼうし",
+        kana: "ボウシ",
+        romaji: "bo u shi",
+        sentence: "ぼうしを かぶります。",
+        img: "boushi_cap.png",
+        lesson: 22
+    },
+    {
+        id: "boku_me",
+        word: "ぼく",
+        kana: "ボク",
+        romaji: "bo ku",
+        sentence: "ぼくは がくせいです。",
+        img: "boku_me.png",
+        lesson: 20
     }
-
 ];
