1f10a99879aa19e94e30035eb3f5df61672a2e9b
yagi3mei <141975956+yagi3mei@users.noreply.github.com>
2026-07-04 10:32:10 +0900

    Improve vocabulary data and prepare Kotoba Sagashi development

--- data/kana/hira/seion/hira_ra.js
+++ data/kana/hira/seion/hira_ra.js
@@ -25,5 +25,23 @@ export default [
     sentence: "らいねん にほんえ いきます",
     img: "rainen_nextyear.png",
     lesson: 5
+  },
+  {
+    id: "reizouko_fridge",
+    word: "れいぞうこ",
+    kana: "レイゾウコ",
+    romaji: "re i zo u ko",
+    sentence: "れいぞうこに みずが あります",
+    img: "reizouko_fridge.png",
+    lesson: 10
+  },
+  {
+    id: "renshuu_practice",
+    word: "れんしゅうします",
+    kana: "レンシュウシマス",
+    romaji: "re n shu u shi ma su",
+    sentence: "サッカーを れんしゅうします",
+    img: "renshuu_practice.png",
+    lesson: 19
   }
 ];
